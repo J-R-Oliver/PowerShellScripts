6083f1e6d7d626fbb333bc378eb340e850a4a29c
James' MacBook Pro <joliver.robert@me.com>
2019-11-06 21:17:03 +0000

    Half way through updating Disabled-OldAdUser

--- Scripts/Disable-OldADUser.ps1
+++ Scripts/Disable-OldADUser.ps1
@@ -1,18 +1,22 @@
-#Import user account CSV
-$UsersToDisable = Import-Csv -Path (Join-Path -Path ([Environment]::GetFolderPath("Desktop")) -ChildPath "VEEAMExcludedUserAccounts.csv") 
+#Create file browser
+Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+    InitialDirectory = [Environment]::GetFolderPath('Desktop')
+    Filter = 'CSV (Comma delimited) (*csv) |*.csv'
+}
+$FileBrowser.ShowDialog()
+
+#Load csv 
+$UsersToDisable = Import-Csv -Path $FileBrowser.FileName
 
 $TodaysDate = Get-Date -Format "dd/MM/yyyy"
 
 foreach ( $User in $UsersToDisable ){
-
-        $UserAccount = Get-AdUser -Filter "UserPrincipalName -eq '$($User.UserPrincipalName)'"
-
-        Set-ADUser -Identity $UserAccount -Add @{msExchHideFromAddressLists="TRUE"} -Description "Disabled on $TodaysDate"                       
-        Disable-ADAccount -Identity $UserAccount
-        Move-ADObject -Identity $UserAccount -TargetPath 'OU=DisabledAccounts,DC=st-annes,DC=org,DC=uk' 
-
-        $User."Account Deleted" = 'Disabled'
-
+    UserAccount = Get-AdUser -Filter "UserPrincipalName -eq '$($User.UserPrincipalName)'"
+    Set-ADUser -Identity $UserAccount -Add @{msExchHideFromAddressLists="TRUE"} -Description "Disabled on $TodaysDate"                       
+    Disable-ADAccount -Identity $UserAccount
+    Move-ADObject -Identity $UserAccount -TargetPath 'OU=DisabledAccounts,DC=st-annes,DC=org,DC=uk' 
+    $User."Account Deleted" = 'Disabled'
 }
 
 #Exports array to spreadsheet 
